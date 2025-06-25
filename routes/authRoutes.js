2e20c06078e605e7557c683a81cddc1c33c43569
Wilgas ADJOVI <wilfrieda216@gmail.com>
2025-06-25 16:43:20 +0100

    middlewares routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const { register, login } = require('../controllers/authController');
+const auth = require('../middlewares/auth');
+const User = require('../models/User');
+
 
 router.post('/register', register);
 router.post('/login', login);
+router.get('/me', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-motDePasse');
+    if (!user) return res.status(404).json({ message: 'Utilisateur non trouvé' });
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json({ message: 'Erreur serveur', error: err });
+  }
+});
 
 module.exports = router;
