3162b1ca4bb74cd3cb539f7189abfa35f1de9cf4
Wilgas ADJOVI <wilfrieda216@gmail.com>
2025-07-07 11:53:18 +0100

    router  commentaire

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -25,4 +25,11 @@ router.get('/leaders', authMiddleware, checkRole(['admin']), listerLeaders);
 // ✅ Membres d’un leader
 router.get('/leaders/:id/membres', authMiddleware, checkRole(['admin']), getMembresDuLeader);
 
+// Leader commente un référent
+router.post('/referents/:referentId/commentaire-leader', authMiddleware, checkRole(['leader']), ajouterCommentaireLeader);
+
+// Admin commente un référent
+router.post('/referents/:referentId/commentaire-admin', authMiddleware, checkRole(['admin']), ajouterCommentaireAdmin);
+
+
 module.exports = router;
