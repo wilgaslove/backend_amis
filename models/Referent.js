a3f4c65e8e2117abb30659481e8e74ef6ed546bd
Wilgas ADJOVI <wilfrieda216@gmail.com>
2025-07-07 10:57:30 +0100

    models/referent

---
+++ models/Referent.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const referentSchema = new Schema({
+  commentaireLeader: String,
+  commentaireAdmin: String, 
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  membres: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Membre'
+    }
+  ]
+}, { timestamps: true });
+
+module.exports = mongoose.model('Referent', referentSchema);
