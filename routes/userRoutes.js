b5a635c1a8dce6e5aaf298b9d7cc6a8afab6eca4
Wilgas ADJOVI <wilfrieda216@gmail.com>
2025-08-28 14:44:36 +0100

    new complete

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,18 +2,14 @@ const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middlewares/authMiddleware');
 const checkRole = require('../middlewares/checkRole');
-const {
-  creerReferent
-} = require('../controllers/referentController');
+const {creerReferent} = require('../controllers/referentController');
 
-const {
-  listerLeaders,
-} = require('../controllers/leaderController');
+const {listerLeaders,} = require('../controllers/leaderController');
 
 // ✅ Liste des leaders
 router.get('/leaders', authMiddleware, checkRole(['admin']), listerLeaders);
 
 // Leader crée un référent
- router.post('/referents/creer', authMiddleware, checkRole(['leader']), creerReferent);
+ router.post('/referents/creer', authMiddleware, checkRole(['leader', 'admin']), creerReferent);
 
 module.exports = router;
