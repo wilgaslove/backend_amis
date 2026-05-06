42ba3f8d54d2185c7666a4fc86e4815898d48831
Wilgas ADJOVI <wilfrieda216@gmail.com>
2026-05-06 20:32:28 +0200

    suppressin dns membre.js

--- models/Membre.js
+++ models/Membre.js
@@ -1,35 +1,5 @@
 const mongoose = require('mongoose');
 
-// const membreSchema = new mongoose.Schema({
-//   firstName: String,
-//   lastName: String,
-//   profession: String,
-//   contact: String,
-//   address: String,
-//   dateArrivee: Date,
-//   baptise: Boolean,
-//   requetePriere: String,
-//   desireRencontrerPasteur: Boolean,
-//   rdvPasteur: Date,
-//   veuxIntegrer: Boolean,
-//   visiteMaison: Boolean,
-//   contactRegulier: Boolean,
-//   veuxFaireBapteme: Boolean,
-//   note: String,
-  
-//   suivi: {
-//     dimanche1: { type: Boolean, default: false },
-//     dimanche2: { type: Boolean, default: false },
-//     dimanche3: { type: Boolean, default: false },
-//     dimanche4: { type: Boolean, default: false }
-//   },
-//   referentId: { 
-//     type: mongoose.Schema.Types.ObjectId, 
-//     ref: 'Referent',  
-//     required: true
-//    },
-  
-// });
 
 const membreSchema = new mongoose.Schema({
   firstName: String,
